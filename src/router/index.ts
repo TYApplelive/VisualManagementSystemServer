77a14ef2061513d2fee2341caabb1b7b632235f4
Applelive <2623999208@qq.com>
2025-02-24 22:49:22 +0800

    增加了一行注释

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,13 @@
-import express from 'express'
+//. 基本路由
+import express from "express"
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello World!')
+router.get("/", (req, res) => {
+    res.send("Hello World!")
 })
 
-router.get('/about', (req, res) => {
-    res.send('You Successful to get about page')
+router.get("/about", (req, res) => {
+    res.send("You Successful to get about page")
 })
 
 export default router
