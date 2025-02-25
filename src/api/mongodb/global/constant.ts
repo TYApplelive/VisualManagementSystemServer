bc4265f261f28be3a05edfad0bc1fbb15ba6e5a5
Applelive <2623999208@qq.com>
2025-02-25 11:01:02 +0800

    保存更改

--- src/api/mongodb/global/constant.ts
+++
@@ -1,6 +0,0 @@
-// . 全局变量
-const url = `mongodb://${process.env.DB_USER_NAME}:${process.env.DB_USER_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-
-const Mongodb_global = { url }
-
-export default Mongodb_global
