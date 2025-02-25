e4e44eee91efd49c3963c20acba8979ddc95a889
Applelive <2623999208@qq.com>
2025-02-25 10:58:02 +0800

    保存更改

--- src/api/mongodb/hooks/db_find.ts
+++ src/api/mongodb/hooks/db_find.ts
@@ -1,31 +1,16 @@
 //. 查找功能
 import { MongoClient } from "mongodb"
-<<<<<<< HEAD
-
-export const findDocuments = async () => {
-    const url = "mongodb://localhost:27017"
-=======
 import Mongodb_global from "../global/constant"
 import type { TypeUser } from "../types"
 
 export const findDocuments = async (query: any) => {
     const url = Mongodb_global.url
->>>>>>> ea6c997af0549d95f0473bc95b7cc1111957a513
     const client = new MongoClient(url)
 
     try {
         await client.connect()
         console.log("Successfully connected to MongoDB")
 
-<<<<<<< HEAD
-        const db = client.db("mydatabase")
-        const collection = db.collection("mycollection")
-
-        const cursor = collection.find()
-        await cursor.forEach((document) => {
-            console.log("Document found:", document)
-        })
-=======
         const db = client.db(process.env.DB_NAME)
         const collection = db.collection(process.env.DB_COLLECTION as string)
 
@@ -39,7 +24,6 @@ export const findDocuments = async (query: any) => {
         for await (const doc of cursor) {
             console.log(doc)
         }
->>>>>>> ea6c997af0549d95f0473bc95b7cc1111957a513
     } catch (error) {
         console.log("Failed to connect to MongoDB", error)
     } finally {
