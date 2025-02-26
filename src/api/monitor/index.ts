189d6b89f7f2a0e009d40cff34c8d02cb72f6019
Applelive <2623999208@qq.com>
2025-02-26 22:43:18 +0800

    别名mqtt

--- src/api/monitor/index.ts
+++ src/api/monitor/index.ts
@@ -1,7 +1,3 @@
 //. monitor 的 api出口
 // * monitor文件用于监听某个数据并对该数据进行归纳操作
-<<<<<<< HEAD
 export * as mqtt from "./mqttClinet"
-=======
-export * from "./server"
->>>>>>> 51e4292b23dd54d15934dacdb9b36d992ac44e1e
