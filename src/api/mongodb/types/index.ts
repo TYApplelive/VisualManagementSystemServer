fda0180c808ef3ad0a2034b08cf227d65a965390
Applelive <2623999208@qq.com>
2025-02-28 20:19:57 +0800

    更改数据库type文件结构

--- src/api/mongodb/types/index.ts
+++ src/api/mongodb/types/index.ts
@@ -1,57 +1,9 @@
 //. 数据库用户数据类型定义
 
 /**
- * 枚举变量
+ * mongodb Define Types
  */
-export enum UserStatus {
-    Registered = "已注册",
-    Unregistered = "未注册",
-    Deleted = "已删除"
-}
-
-export enum UserRole {
-    Admin = "管理员",
-    User = "普通用户"
-}
-
-interface _weak_TypeUserInfo {
-    email?: string
-}
-
-interface TypeUserInfo {
-    email: string
-}
-
-/**
- * 强类型用户数据类型定义
- * 用于完全数据参数传入
- * 例如插入数据
- */
-export interface TypeUser {
-    account: string
-    password: string
-    user_info: TypeUserInfo
-    create_time: string
-    id: string
-    status: UserStatus
-    role: UserRole
-}
-
-/**
- * 弱类型用户数据类型定义
- * 用于不完全数据参数传入
- * 例如更新某个数据，查找字段
- */
-export interface _weak_TypeUser {
-    account?: string
-    password?: string
-    user_info?: _weak_TypeUserInfo
-    create_time?: string
-    id?: string
-    status?: UserStatus
-    role?: UserRole
-    [key: string]: any
-}
+export * from "./db_type"
 
 /**
  * mongodb hooks return
