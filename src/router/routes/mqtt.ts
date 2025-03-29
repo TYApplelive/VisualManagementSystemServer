bb63646f70ac379f9ea5feccd111f17674a99422
_Asialing_ <96574207+TYApplelive@users.noreply.github.com>
2025-03-29 16:26:57 +0800

    优化后端返回api格式

--- src/router/routes/mqtt.ts
+++ src/router/routes/mqtt.ts
@@ -1,26 +1,28 @@
 // Mqtt 操作函数
 import express from "express"
 import * as mqtt from "@/api/monitor/mqttClinet"
+import { routerResponeHandle } from "@/router/types/router_return"
+
 const router = express.Router()
 router.get("/", (req, res) => {
-    res.send("Mqtt Router!")
+    res.send(routerResponeHandle("Mqtt Router Test!", true, "Successful!"))
 })
 
 router.get("/open", (req, res) => {
     var topic = process.env.MQTT_TOPIC_Device1
     if (topic == undefined) {
-        res.send("Mqtt Topic is undefined!")
+        res.send(routerResponeHandle("Mqtt Topic is undefined!", false))
         return
     }
     // 打开mqtt客户端并订阅主题
     mqtt.connectMqttClient(topic)
-    res.send("Mqtt Open!")
+    res.send(routerResponeHandle("Mqtt Open!", true))
 })
 
 router.get("/close", (req, res) => {
     // 关闭mqtt客户端
     mqtt.disconnectMqttClient()
-    res.send("Mqtt Close!")
+    res.send(routerResponeHandle("Mqtt Close!", true))
 })
 
 export default router
